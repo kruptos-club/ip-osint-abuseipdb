21843ab2baf9d2e1f7bd2e04dcebc0e0c528bb59
Naveen S <naveen2aug@gmail.com>
2018-12-20 13:15:46 +0530

    Update ip_info.php

--- ip_info.php
+++ ip_info.php
@@ -8,6 +8,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
     $result = curl_exec($ch);
+    $_SESSION['res'] = $result;
     if (curl_errno($ch)) {
         echo 'Error:' . curl_error($ch);
     }
@@ -19,6 +20,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 $result2 = $_SESSION['result1'];
 $ip = $_SESSION['url'];
+$res = $_SESSION['res'];
 ?>
 <section>
     <h1>OSINT IP info</h1>
@@ -44,17 +46,19 @@ $ip = $_SESSION['url'];
 </section>
 <section>
     <?php
-    if ($result2) {
-        echo
-            '
-<h4>Details of IP ' . $result2["ip"] . ' are,</h4><br/>
+    if ($res) {
+        if ($result2) {
+            echo
+                '
+<h4>Details of IP <strong>' . $result2["ip"] . '</strong> are,</h4><br/>
 <h4>The IP with ISO code <strong>&quot;' . $result2["isoCode"] . '&quot;</strong> belongs to <strong>' . $result2["country"] . '</strong></h4><br/>';
-        echo '<h4>The IP is <strong>';  echo $result2["isWhitelisted"] ? 'Whitelisted' : 'Blacklisted' ; echo '</strong></h4><br/>';
-        echo '<h4>Abuse level Confidence score is <strong>' . $result2["abuseConfidenceScore"] . '</strong></h4><br/><br/>';
-        echo '<p>Confidence of abuse is a rating (scaled 0-100) of how confident we are, based on user reports, that an IP address is entirely malicious. So a rating of 100 means we are sure an IP address is malicious, while a rating of 0 means we have no reason to suspect it is malicious.</p>';
-    }
-    else {
-        echo '<h4>Details of IP <strong>' . $ip . '</strong> not found</h4><br/><br/>';
+            echo '<h4>The IP is <strong>';  echo $result2["isWhitelisted"] ? 'Whitelisted' : 'Blacklisted' ; echo '</strong></h4><br/>';
+            echo '<h4>Abuse level Confidence score is <strong>' . $result2["abuseConfidenceScore"] . '</strong></h4><br/><br/>';
+            echo '<p>Confidence of abuse is a rating (scaled 0-100) of how confident we are, based on user reports, that an IP address is entirely malicious. So a rating of 100 means we are sure an IP address is malicious, while a rating of 0 means we have no reason to suspect it is malicious.</p><br/><br/>';
+        }
+        else {
+            echo '<h4>Details of IP <strong>' . $ip . '</strong> not found</h4><br/><br/>';
+        }
     }
     ?>
 </section>
